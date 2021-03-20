23a1fa26c965b4f019fc1a8a28e698886e2af9d8
iamzeyadzidan <81028196+iamzeyadzidan@users.noreply.github.com>
2021-03-20 23:02:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
-
-app.listen(() => console.log(" Quran Bot Start "));
-app.use('/ping', (req, res) => {
-  res.send(new Date());
-});
+app.listen(() => console.log("Quran Bot Start"));
 
 const { TOKEN, CHANNEL, SERVER, STATUS, LIVE } = require("./config.json");
 const discord = require("discord.js");
@@ -12,9 +8,7 @@ const client = new discord.Client();
 const ytdl = require('ytdl-core');
 
 client.on('ready', async () => {
-  client.user.setActivity(STATUS)
   let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
-
   if(!channel) return;
   const connection = await channel.join();
   connection.play(ytdl(LIVE))
